8a1dcf01e90c7a291ce727407c439f8161ac7cd2
alihani2002 <alyhani2002@gmail.com>
2026-05-15 20:44:46 +0300

    database

--- Resturant.Infrastructure/Data/AppDbContextFactory.cs
+++ Resturant.Infrastructure/Data/AppDbContextFactory.cs
@@ -8,7 +8,7 @@ namespace Resturant.Infrastructure.Data
         public AppDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer("Server=db40463.public.databaseasp.net; Database=db40463; User Id=db40463; Password=5y%EQ7e=o@3J; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True;  ");
+            optionsBuilder.UseSqlServer("Server=.;Database=ResturantDb;Trusted_Connection=True;TrustServerCertificate=True;");
 
             return new AppDbContext(optionsBuilder.Options);
         }
